8b1809e39033f4c06b35966f5d7f6a3eb04b6fe2
aarushikapoor2004 <aarushik250@gmail.com>
2026-04-09 00:18:56 +0530

    fix middleware path

--- routes/schoolRoutes.js
+++ routes/schoolRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { addSchool, listSchools } = require("../controllers/school");
-const { validateSchoolInput } = require("../middleware/validate");
+const { validateSchoolInput } = require("../middleware/middleware");
 
 router.post("/addSchool", validateSchoolInput, addSchool);
 router.get("/listSchools", listSchools);
