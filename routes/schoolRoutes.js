cf957b7b86548639477e1e49137ef48109fc183d
aarushikapoor2004 <aarushik250@gmail.com>
2026-04-09 00:12:37 +0530

    fix controller path

--- routes/schoolRoutes.js
+++ routes/schoolRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { addSchool, listSchools } = require("../controllers/schoolController");
+const { addSchool, listSchools } = require("../controllers/school");
 const { validateSchoolInput } = require("../middleware/validate");
 
 router.post("/addSchool", validateSchoolInput, addSchool);
